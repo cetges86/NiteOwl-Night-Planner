653d4cadaeb7704df7cafc1c332dec19f6fdb463
Trevor Ogg <trevorogg@Trevors-MacBook-Air.local>
2018-03-17 13:41:16 -0600

    added geolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    
+    var myLat;
+    var myLong;
+    
+    function displayCards(type) {
+
+    }
+
+    function findLocation() {
+
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        };
+
+        function success(pos) {
+            var crd = pos.coords;
+
+            myLat = crd.latitude;
+            myLong = crd.longitude;
+
+            console.log("my latitude: " + crd.latitude);
+            console.log("my longitude: " + crd.longitude);
+        };
+
+        function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+
+    }
+
+    findLocation();
 
     $('#icons').hide();
 
+    $('#submit').click(function () {
+        event.preventDefault();
+        var name = $('#first_name').val().trim();
 
-    
-$('#submit').click(function(){
-    event.preventDefault();
-    var name = $('#first_name').val().trim();
+        var email = $('#email').val().trim();
 
-    var email = $('#email').val().trim();
-
-    console.log(name);
-    console.log(email);
+        console.log(name);
+        console.log(email);
 
 
-    $('#inputs').remove();
-    $('#icons').show(1500);
-})
+        $('#inputs').remove();
+        $('#icons').show(1500);
+    })
 
-
-function displayCards(type){
-
-    
-}
-
+    $('body').on('click', '#food', function (event) {
+        alert("clicked on food!");
+    });
 
 });
