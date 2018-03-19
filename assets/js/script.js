60f6e9fb56458522c756c832348c0b32ad73201f
Christopher Etges <Chris@MacBook-Pro.local>
2018-03-18 21:03:11 -0600

    cards display info

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,31 @@ $(document).ready(function () {
     $('#icons').hide();
     $('#card').hide();
 
-    function displayCards(type) {
-        $('#icons').remove();
-        $('#card').show(1500);
+    // function displayCards(type) {
+    //     $('#icons').hide();
 
-    }
+    //     var restInfo = {
+    //         name:response[i].name,
+    //         address:response[i].location.address,
+    //         cuisine:response[i].cuisines,
+    //         menuLink:response[i].menu_url,
+    //         rating:response[i].user_rating.aggregate_rating,
+    //         cost:response[i].average_cost_for_two,
+    //         photo:response[i].photos_url
+    //     }
+    //     $('#name').text(restInfo.name);
+    //     var list = $('<ul>');
+    //     $(list).append(`<li>Address:${restInfo.address}</li>
+    //     <li>Cuisine:${restInfo.cuisine}</li>
+    //     <li>Average Cost for Two:${restInfo.cuisine}</li>
+    //     <li>Avg. Rating: ${restInfo.rating}</li>`);
+
+    //     $('#info').append(list);
+    //     $('#image').attr('src',restInfo.photo);
+    //     $('#link').attr('href',restInfo.menuLink);
+    //     $('#card').show(1500);
+
+    // }
 
     function findLocation() {
 
@@ -39,40 +59,70 @@ $(document).ready(function () {
     }
 
     findLocation();
-    
+
     $('#icons').hide();
-    
+
     $('#submit').click(function () {
         event.preventDefault();
         var name = $('#first_name').val().trim();
-        
+
         var email = $('#email').val().trim();
-        
+
         console.log(name);
         console.log(email);
         var nameDisplay = (`<h5> ${name} </h5>`);
         $('#nameDisplay').append(nameDisplay).fadeIn(2000);
-        
-        
-        $('#inputs').remove();
+
+
+        $('#inputs').hide();
         $('#icons').show(1500);
+        setTimeout(restaurantsInfo,1500);
     })
 
 
-    $('.fas').on('click', function () {
-        var type = $(this).attr('id')
-        displayCards(this);
-    })
 
-function restaurantsInfo(){
-    var queryURL = 'https://developers.zomato.com/api/v2.1/search?lat=' + myLat + '&lon=' + myLong + '&apikey=1186480d6decb5529b6df0ca0c638be9'
-    $.ajax({
-        url: queryURL,
-        method: "Get"
-    }).then(function(response){
-        console.log(response)
-    });
-};
-restaurantsInfo();
+    function restaurantsInfo() {
+        var queryURL = 'https://developers.zomato.com/api/v2.1/search?lat=' + myLat + '&lon=' + myLong + '&apikey=1186480d6decb5529b6df0ca0c638be9'
+        $.ajax({
+            url: queryURL,
+            method: "Get"
+        }).then(function (response) {
+            $('.fas').on('click', function () {
+                var type = $(this).attr('id')
+                $('#icons').hide();
+
+                for (var i = 0; i < 10; i++) {
+
+                    var restInfo = {
+                        name: response.restaurants[i].restaurant.name,
+                        address: response.restaurants[i].restaurant.location.address,
+                        cuisine: response.restaurants[i].restaurant.cuisines,
+                        menuLink: response.restaurants[i].restaurant.menu_url,
+                        rating: response.restaurants[i].restaurant.user_rating.aggregate_rating,
+                        cost: response.restaurants[i].restaurant.average_cost_for_two,
+                        photo: response.restaurants[i].restaurant.thumb
+                    };
+
+                    $('.name').text(restInfo.name);
+                    var list = $('<ul>');
+                    $(list).append(`<li>Address: ${restInfo.address}</li>
+                        <li>Cuisine: ${restInfo.cuisine}</li>
+                        <li>Average Cost for Two: ${restInfo.cost}</li>
+                        <li>Avg. Rating: ${restInfo.rating}</li>`);
+                    $('#info').html(list);
+                    $('#image').attr('src', restInfo.photo);
+                    $('#link').attr('href', restInfo.menuLink);
+
+                    $('#card').appendTo('#card-display');
+
+                    $('#card').show(1500);
+
+
+                }
+            })
+            console.log(response)
+        });
+    };
+ 
 
 });
