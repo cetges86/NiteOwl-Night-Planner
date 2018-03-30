c47def34dc985fa0156f329ab9211ac9525dae03
Christopher Etges <Chris@MacBook-Pro.local>
2018-03-29 20:13:28 -0600

    modal

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,97 +1,38 @@
+var nightInfo = [
+    {
+        info: "15110 East Hampden Avenue",
+        name: "Dry Dock Brewing Co."
+    },
+    {
+        info: "2376 15th Street, Denver 80202",
+        name: "My Brother's Bar"
+    },
+    {
+        info: "PG-13, 2H20M",
+        name: "Ready Player One"
+    }
+]
+
+for (var i = 0; i < nightInfo.length; i++) {
+    var message = $('<li>')
+    message.append(`<ul>
+<h1>Your first event:${nightInfo[i].name}</h1>
+<li> ${nightInfo[i].info}</li></ul>
+`)
 
 
 
-var myCity = "denver";
-    var brewApi = "ff0222dd8fe6c591c1c40a9656a717d8/"
-    //brewery needs city name
-    // created brewApi local variable 
-
-    function breweryInfo() {
-        zipToLocation();
-        console.log(enteredCity);
-        var queryURL = 'https://beermapping.com/webservice/loccity/' + brewApi + enteredCity + '&s=json'
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function (response) {
-            console.log(response);
+}
 
 
-            $('#beer').on('click', function (event) {
-                zipToLocation();
-                var type = $(this).attr('id')
-                $('#icons').hide();
-                $('#card-display').hide();
-
-                for (var i = 0; i < 20; i++)
-                    // if statement excluding "Beer Stores" from displaying
-                    // if(response[i].status(0)=== "Beer Store") {
-
-
-
-                var placeId = 19866;
-                var queryURL = 'http://beermapping.com/webservice/locimage/' + brewApi + placeId + '&s=json'
-                $.ajax({
-                    url: queryURL,
-                    method: 'GET'
-                }).then(function (response) {
-                    console.log(response)
-
-                    displayCards(i);
+var obj = {
+    timeStamp: 1522371650283,
+    userEmail:
+        "c.etges86@gmail.com" <,
+    userName:
+        "Chris",
+    userZip:
+        "80015"
+}
 
 
-                    function displayCards(i) {
-                        var brewInfo = {
-                            name: response[i].name,
-                            street: response[i].street,
-                            city: response[i].city,
-                            state: response[i].state,
-                            review: response[i].overall,
-                            URL: response[i].url,
-                            image: response[i].imageCount
-                        };
-
-                        var newCard = $('<div>');
-                        newCard.addClass('newCard', 'col', 's4');
-
-                        newCard.append(`<div class="card small">
-                                  <div class="card-image waves-effect waves-block waves-light">
-                                 <img id="image" class="activator" src="assets/images/nuts.jpg">
-                             </div>
-                             <div class="card-content">
-                                 <span class="card-title activator grey-text text-darken-4">${brewInfo.name}
-                                     <i class="material-icons right">more_vert</i>
-                                 </span>
-                                 <p>
-                                     <a target="_blank" href="${brewInfo.review}">Reviews</a>
-                                 </p>
-                             </div>
-                             <div class="card-reveal">
-                                 <span class="card-title grey-text text-darken-4">${brewInfo.name}
-                                     <i class="material-icons right">close</i>
-                                 </span>
-                            <ul>
-                                        <li>Address: ${brewInfo.street}<br>
-                                        ${brewInfo.city}, ${brewInfo.state}</li>
-                                    <li>Reviews: ${brewInfo.review}</li>
-
-                                    <li>Website: <a target= "_blank" href="http://www.${brewInfo.URL}">Link</a></li>
-
-                                    <i id="plus" data-name="${brewInfo.name}" data-addr = "${brewInfo.street}"class="right-align material-icons addButton">add_circle</i>
-                                   </ul>
-                                   </div>
-                                       </div>`);
-
-
-                        newCard.appendTo('#card-display');
-
-                    }
-
-                    $('#card-display').show(2000);
-                    $('#back').show();
-                    goBack();
-                });
-            })
-        }
-        )
-    };
